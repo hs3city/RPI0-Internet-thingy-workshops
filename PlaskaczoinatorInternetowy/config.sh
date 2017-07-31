63237637c4ac8bcfc37597b10c4be4a0dd569fe7
mroz <private@mrozo.pl>
2017-07-31 18:38:23 +0200

    updated configuration for the PlaskaczoinatorInternetowy project

--- PlaskaczoinatorInternetowy/config.sh
+++ PlaskaczoinatorInternetowy/config.sh
@@ -5,5 +5,5 @@
 export PI_USER=pi
 export PI_PASS=hs
 export PI_HOST="mroz3-pi0.local"
-export LOCAL_PROJECT_PATH="/home/mroz/Workspace/RPI0-Internet-thingy-workshops/PlaskaczoinatorInternetowy"
+export LOCAL_PROJECT_PATH="$HOME""/RPI0-Internet-thingy-workshops/PlaskaczoinatorInternetowy"
 export TARGET_FOLDER="/home/pi/wichajstry"
