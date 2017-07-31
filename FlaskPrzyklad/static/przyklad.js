16059f0d895984fc1be89271cb406cb0dda6678a
mroz <private@mrozo.pl>
2017-07-31 21:25:04 +0200

    fixed typo

--- FlaskPrzyklad/static/przyklad.js
+++ FlaskPrzyklad/static/przyklad.js
@@ -2,7 +2,7 @@ function zwieksz_licznik(){
   $.ajax({
     url: "/licznik",
     type: "POST",
-    data: JSON.stringify({"browser": navigator.userAgent, "add": 5),
+    data: JSON.stringify({"browser": navigator.userAgent, "add": 5}),
     async: false,
     success: function(data) {
       alert(data)
